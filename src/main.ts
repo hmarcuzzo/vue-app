227e223620b11c3e67c59a66584aaf82193c2ab8
Henrique Marcuzzo <henrique.marcuzzo@coop.co.uk>
2025-05-18 18:15:26 +0100

    ✨ Add Ant Design Vue library; integrate styles and update main application entry point

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
+import Antd from "ant-design-vue";
 import { createApp } from "vue";
+import "ant-design-vue/dist/reset.css";
 
 import { routerConfig } from "@/router/router.config.ts";
 
 import App from "./App.vue";
 
-createApp(App).use(routerConfig).mount("#app");
+createApp(App).use(routerConfig).use(Antd).mount("#app");
