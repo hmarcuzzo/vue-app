c958076c69d8420b18a090d70736a779c2d2e6d0
Henrique Marcuzzo <henrique.marcuzzo@coop.co.uk>
2025-05-22 12:19:45 +0100

    ✨ Update color and typography configurations

--- src/config/theme.config.ts
+++ src/config/theme.config.ts
@@ -1,18 +1,20 @@
 import type { ThemeConfig } from "ant-design-vue/es/config-provider/context";
 
+import colours from "@/assets/modules/_colours.module.scss";
+import typography from "@/assets/modules/_typography.module.scss";
+
 export const themeConfig: ThemeConfig = {
   token: {
-    colorPrimary: "#219EBC",
-    colorSuccess: "#4CAF50",
-    colorWarning: "#FFB703",
-    colorError: "#FF5252",
-    colorInfo: "#219EBC",
-    colorTextBase: "#282828",
-    colorTextSecondary: "rgba(2, 48, 71, 0.7)",
-    colorBgBase: "#FFFFFF",
-    colorBgContainer: "#FFFFFF",
-    colorBorder: "rgba(2, 48, 71, 0.1)",
-    fontFamily:
-      "'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'",
+    colorPrimary: colours.colorPrimary,
+    colorSuccess: colours.colorSuccess,
+    colorWarning: colours.colorWarning,
+    colorError: colours.colorError,
+    colorInfo: colours.colorInfo,
+    colorTextBase: colours.colorTextPrimary,
+    colorTextSecondary: colours.colorTextMuted,
+    colorBgBase: colours.colorBgPrimary,
+    colorBgContainer: colours.colorBgPrimary,
+    colorBorder: colours.colorBorder,
+    fontFamily: typography.fontFamily,
   },
 };
