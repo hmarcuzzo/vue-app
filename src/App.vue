d1aa60a2612f96e1617d561a054565630e1145d7
Henrique Marcuzzo <henrique.souza.m06@gmail.com>
2025-06-23 18:30:56 +0100

    ✨ Create DefaultLayout component to streamline App.vue structure

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
-import AppFooter from "@/components/base/AppFooter.vue";
-import AppHeader from "@/components/base/AppHeader/AppHeader.vue";
-import AppSidebar from "@/components/base/AppSidebar.vue";
 import { useThemeConfig } from "@/core/config/theme.config";
+import DefaultLayout from "@/layouts/DefaultLayout.vue";
 import { useAppMetadataStore } from "@/stores/appMetadata.store";
 
 const appMetadataStore = useAppMetadataStore();
@@ -13,47 +11,10 @@ const { themeConfig } = useThemeConfig();
 
 <template>
   <a-config-provider :theme="themeConfig">
-    <div class="app-container">
-      <AppHeader />
-
-      <AppSidebar />
-      <main class="main-layout">
-        <div class="main-content">
-          <router-view />
-        </div>
-      </main>
-      <AppFooter />
-    </div>
+    <DefaultLayout />
   </a-config-provider>
 </template>
 
 <style lang="scss">
 @use "@/assets/scss/index" as *;
-
-.app-container {
-  min-height: 100dvh;
-  display: flex;
-  flex-direction: column;
-}
-
-.main-layout {
-  display: flex;
-  flex: 1 1 auto;
-  margin-left: 0;
-}
-
-.main-content {
-  flex: 1;
-  margin: 48px 24px;
-}
-
-@media (min-width: $breakpoint-desktop-large) {
-  .app-container:has(.sidebar-container[aria-expanded="false"]) .main-layout {
-    margin-left: calc($collapsed-sidebar-width - 8px);
-  }
-
-  .app-container:has(.sidebar-container[aria-expanded="true"]) .main-layout {
-    margin-left: $expanded-sidebar-width;
-  }
-}
 </style>
---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import AppFooter from "@/components/base/AppFooter.vue";
+import AppHeader from "@/components/base/AppHeader/AppHeader.vue";
+import AppSidebar from "@/components/base/AppSidebar.vue";
+</script>
+
+<template>
+  <div class="app-container">
+    <AppHeader />
+
+    <AppSidebar />
+    <main class="main-layout">
+      <div class="main-content">
+        <router-view />
+      </div>
+    </main>
+    <AppFooter />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.app-container {
+  min-height: 100dvh;
+  display: flex;
+  flex-direction: column;
+}
+
+.main-layout {
+  display: flex;
+  flex: 1 1 auto;
+  margin-left: 0;
+}
+
+.main-content {
+  flex: 1;
+  margin: 48px 24px;
+}
+
+@media (min-width: $breakpoint-desktop-large) {
+  .app-container:has(.sidebar-container[aria-expanded="false"]) .main-layout {
+    margin-left: calc($collapsed-sidebar-width - 8px);
+  }
+
+  .app-container:has(.sidebar-container[aria-expanded="true"]) .main-layout {
+    margin-left: $expanded-sidebar-width;
+  }
+}
+</style>
